5cb7a244d1faa184d72f7738213fd4fd95ec0d37
Vani <v.tscheak@web.de>
2019-05-10 09:14:55 +0200

    Jetzt läuft es

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    name="Vani"
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'name': name})
+#ruft funktion post list auf aus ordner views
+#render= dargestellt werden
